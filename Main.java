0ac2435ef19c96c60b54979648c04488624473d6
alena <misamistake@>
2024-03-15 23:56:29 +0300

    did

--- Main.java
+++ Main.java
@@ -1,7 +1,26 @@
 public class Main {
-
-
     public static void main ( String[] args ) {
-        System.out.println ( "Hello world!" );
+        checkLoginAndPassword ( 90, 666, 666 );
+    }
+
+    public static void checkLoginAndPassword ( String login,
+                                               String password, String confirmPassword ) {
+        if ( login.length ( ) > 20 ) {
+            throw new WrongLoginException ( "слишком длинный логин" );
+        }
+        if ( !login.matches ( "[a-zA-z0-9_]+" ) ) {
+            throw new WrongLoginException ( "недопустимые символы" );
+        }
+        if ( !password.matches ( "[a-zA-z0-9_]+" ) ) {
+            throw new WrongPasswordException ( "недопустимые символы" );
+        }
+        if ( password.length ( ) > 20 ) {
+            throw new WrongPasswordException ( "слишком длинный пароль" );
+        }
+        if ( !password.equals ( confirmPassword ) ) {
+            throw new WrongPasswordException ( "пароли не совпадают" );
+        }
+
     }
 }
+
